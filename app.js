09f8e4f450dbdd115a4778836d29d82a291192e0
Abdulrahim Klis <abdulrahim.klis@walter.ba>
2019-01-06 15:50:53 +0100

    added smtng new

--- app.js
+++ app.js
@@ -8,23 +8,24 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 // const app express
 const app = express();
+// use cors
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+//initialize routes
+app.use('/api', require('./routes/api'));
 
 // localhost: 3000
 app.set('port', 3000);
-
-// use cors
-app.use(cors());
 
 mongoose.connect("mongodb://localhost:27017/locations", { useNewUrlParser: true });
 
 // use body parser
 app.use(bodyParser.json());
 
-//initialize routes
-app.use('/api', require('./routes/api'));
-
 // error handling middelweare
 app.use(function (err, req, res, next) {
-    // console.log(err);
-    res.status(422).send({error: err.message});
+    console.log(err);
 });
